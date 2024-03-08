16e9fee6d23794048e81b6636628bc8be9d69976
W1ndys <w1ndys@outlook.com>
2024-03-08 19:28:57 +0800

    脚本增加默认开启评论

--- 一键新建博客.py
+++ 一键新建博客.py
@@ -10,6 +10,7 @@ file_path = os.path.join("docs", "update", "posts", f"{current_date}.md")
 # 创建md文件并写入元数据
 with open(file_path, "w", encoding="utf-8") as file:
     file.write(f"---\n")
+    file.write(f"comments: true\n")
     file.write(f"date: {current_date}\n")
     file.write(f"---\n\n")
     file.write(f"# {current_date} 更新日志\n\n")
