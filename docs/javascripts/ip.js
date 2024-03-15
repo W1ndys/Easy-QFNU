c503c6cba5a057bdd1ea06b1da42c0964413516d
W1ndys <w1ndys@outlook.com>
2024-03-15 13:30:57 +0800

    修复图片样式覆盖

--- docs/javascripts/ip.js
+++ docs/javascripts/ip.js
@@ -8,20 +8,8 @@ if (!isMobileDevice()) {
     // 创建图像元素
     var img = document.createElement("img");
     img.src = "https://tool.lu/netcard/"; // 设置图像的源
-    img.alt = "Your Image"; // 图像的替代文本
-
-    // 设置图像样式
-    img.style.position = "fixed";
-    img.style.bottom = "0";
-    img.style.right = "0";
-    img.style.zIndex = "999"; // 确保图片在最上层
-    img.style.animationName = "slideIn"; // 应用入场动画
-    img.style.animationDuration = "1s";
-    img.style.animationTimingFunction = "ease-in-out";
-
-    // 设置图片大小
-    img.style.width = "310px"; // 设置图片宽度
-    img.style.height = "auto"; // 设置高度自动调整，保持宽高比
+    img.alt = "IP image"; // 图像的替代文本
+    img.className = "custom-image"; // 添加自定义类名
 
     // 将图像添加到 body 中
     document.body.appendChild(img);
