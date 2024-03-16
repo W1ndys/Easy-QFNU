d409dc5c5c3bd513dc7e953573c29a167c79f25d
W1ndys <w1ndys@outlook.com>
2024-03-16 22:15:58 +0800

    修改生成更新日志的脚本

--- New-update.py
+++ New-update.py
@@ -2,15 +2,14 @@ import os
 from datetime import datetime
 
 """
-主程序：创建并打开一个Markdown文件以记录当前时间的更新日志。
+主程序：创建并打开一个Markdown文件以记录每天的更新日志。
 """
 
-# 获取当前日期和时间，并格式化为年月日时分的字符串
-current_datetime = datetime.now()
-formatted_datetime = current_datetime.strftime("%Y年%m月%d日%H时%M分")
+# 获取当前日期，并格式化为年月日的字符串
+current_date = datetime.now().strftime("%Y年%m月%d日")
 
-# 构建文件路径，位于docs/update/posts目录下，文件名格式为"年月日时分.md"
-file_path = os.path.join("docs", "update", "posts", f"{formatted_datetime}.md")
+# 构建文件路径，位于docs/update/posts目录下，文件名格式为"年月日.md"
+file_path = os.path.join("docs", "update", "posts", f"{current_date}.md")
 
 # 检查文件是否已存在；若存在，则直接打开文件
 if os.path.exists(file_path):
@@ -22,10 +21,10 @@ else:
         file.write(f"---\n")
         file.write(f"comments: true\n")
         file.write(f"authors: [W1ndys]\n")
-        file.write(f"date: {current_datetime.strftime('%Y-%m-%d %H:%M:%S')}\n")
+        file.write(f"date: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         file.write(f"---\n\n")
         # 写入更新日志的标题
-        file.write(f"# {formatted_datetime} 更新日志\n\n")
+        file.write(f"# {current_date} 更新日志\n\n")
         # 添加分隔符，用于内容扩展
         file.write(f"\n\n<!-- more -->")
 
