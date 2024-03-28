fcfbfc5b78451cbdd3593794c7cca011c59c0229
W1ndys <w1ndys@outlook.com>
2024-03-28 23:23:26 +0800

    优化脚本颜色，排除列表

--- 批量打开评论.py
+++ 批量打开评论.py
@@ -1,8 +1,24 @@
 import os
 import re
 
+# ANSI 转义码
+BLUE = "\033[94m"
+GREEN = "\033[92m"
+ENDC = "\033[0m"
 
-def add_comments_true(file_path):
+
+def add_comments_true(file_path, docs_dir):
+    # 构建排除文件的路径列表
+    exclude_paths = [
+        os.path.join(docs_dir, "嵌入测试.md"),
+        os.path.join(docs_dir, "update", "index.md"),
+    ]
+
+    # 排除不需要修改的文件
+    if file_path in exclude_paths:
+        print(f"{BLUE}跳过文件: {os.path.relpath(file_path)}{ENDC}")
+        return
+
     with open(file_path, "r+", encoding="utf-8") as file:
         content = file.read()
         match = re.search(r"^---\n(.*?)\n---\n", content, re.DOTALL)
@@ -17,13 +33,13 @@ def add_comments_true(file_path):
             file.seek(0)
             file.write(updated_content)
             file.truncate()
-            print(f"{os.path.relpath(file_path)} 文件已执行完毕")
+            print(f"{GREEN}{os.path.relpath(file_path)} 文件已执行完毕{ENDC}")
         else:
             updated_content = "---\ncomments: true\n---\n" + content
             file.seek(0)
             file.write(updated_content)
             file.truncate()
-            print(f"{os.path.relpath(file_path)} 文件已执行完毕")
+            print(f"{GREEN}{os.path.relpath(file_path)} 文件已执行完毕{ENDC}")
 
 
 def main():
@@ -32,13 +48,14 @@ def main():
     )
     input()
     docs_dir = os.path.join(os.getcwd(), "docs")
+
     for root, dirs, files in os.walk(docs_dir):
         for file_name in files:
             if file_name.endswith(".md"):
                 file_path = os.path.join(root, file_name)
-                add_comments_true(file_path)
+                add_comments_true(file_path, docs_dir)
 
-    input("全部执行完毕，按Enter退出")
+    input("全部执行完毕，请按 Ctrl+C 退出")
 
 
 if __name__ == "__main__":
