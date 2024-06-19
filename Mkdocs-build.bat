c7a2dbe0cef9a84e4c79218674588d317c3283af
W1ndys <w1ndys@outlook.com>
2024-06-19 13:54:04 +0800

    feat: 添加构建成功后自动打开文件夹功能

--- Mkdocs-build.bat
+++ Mkdocs-build.bat
@@ -1,1 +1,24 @@
+@echo off
+
+REM Build the MkDocs site
 mkdocs build
+IF %ERRORLEVEL% NEQ 0 (
+    echo MkDocs build failed!
+    exit /b %ERRORLEVEL%
+)
+
+REM Change directory to the 'site' folder
+cd site
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to change directory to 'site'!
+    exit /b %ERRORLEVEL%
+)
+
+REM Open the 'site' folder in Explorer
+explorer %cd%
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to open the 'site' folder in Explorer!
+    exit /b %ERRORLEVEL%
+)
+
+echo Build and directory change successful!
