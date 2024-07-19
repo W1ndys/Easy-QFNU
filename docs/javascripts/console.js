ba7c6e413dc753645f1ab62aa62d09e641e1281f
W1ndys <w1ndys@outlook.com>
2024-07-19 19:41:16 +0800

    添加控制台输出脚本

---
+++ docs/javascripts/console.js
@@ -0,0 +1,4 @@
+console.log("Hello!");
+console.log("Welcome to Easy-QFNU!");
+console.log(`欢迎来到Easy-QFNU!\nhttps://github.com/W1ndys/Easy-QFNU`);
+console.log("内容反馈：https://blog.w1ndys.top/html/QQ");
