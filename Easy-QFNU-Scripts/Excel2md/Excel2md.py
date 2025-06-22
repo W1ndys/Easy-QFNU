5d4cce47f750250d575d81900e2308eef9264ca4
W1ndys <w1ndys@outlook.com>
2025-06-22 16:15:45 +0800

    fix(Excel2md): 修改数据列名称以反映年份信息
    将数据框中的“学期”列名称更改为“年份”，并在更新日志和
    生成的Markdown内容中相应调整，以确保信息的一致性和准确性。

--- Easy-QFNU-Scripts/Excel2md/Excel2md.py
+++ Easy-QFNU-Scripts/Excel2md/Excel2md.py
@@ -31,7 +31,7 @@ def generate_markdown(data_file):
     df = pd.read_excel(data_file, header=None)
 
     # 为列指定名称
-    df.columns = ["课程名称", "任课老师", "校区", "学期", "老师描述", "提交者昵称"]
+    df.columns = ["课程名称", "任课老师", "校区", "年份", "老师描述", "提交者昵称"]
 
     # 标准化教师名称
     df["任课老师"] = df["任课老师"].apply(normalize_teacher_name)
@@ -56,14 +56,14 @@ def generate_markdown(data_file):
     # 遍历每一行数据
     for _, row in df.iterrows():
         # 添加到更新日志
-        update_log += f"[{row['课程名称']}]-[{row['校区']}]-[{row['任课老师']}]-[{row['提交者昵称']}]-[{row['学期']}年]\n"
+        update_log += f"[{row['课程名称']}]-[{row['校区']}]-[{row['任课老师']}]-[{row['提交者昵称']}]-[{row['年份']}年]\n"
 
         # 生成主要内容
         markdown += f"## {row['课程名称']}\n\n"
         markdown += f"### {row['校区']}\n\n"
         markdown += f"#### {row['任课老师']}\n\n"
         markdown += f"{row['老师描述']}\n\n"
-        markdown += f"> {row['提交者昵称']}({row['学期']})\n\n"
+        markdown += f"> {row['提交者昵称']}({row['年份']})\n\n"
 
     update_log += "\n---\n"
 
